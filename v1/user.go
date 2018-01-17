be9bbde5806463d9ebafd0ea6eb2c3ab57c0f948
stackcats <stackcats@foxmail.com>
2018-01-17 16:06:13 +0800

    add res

---
+++ v1/user.go
@@ -0,0 +1,44 @@
+package v1
+
+import (
+	"net/url"
+	"time"
+
+	"github.com/launchain/api"
+)
+
+// User ...
+type User struct {
+	uri string
+}
+
+// UserRequest ...
+type UserRequest struct {
+	ID             string    `json:"_id"`
+	Authentication int       `json:"authentication"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+	Phone          string    `json:"phone"`
+}
+
+// NewUser ...
+func NewUser(c *api.Config) *User {
+	uri := "http://" + c.Host + ":" + c.Port
+	return &User{uri: uri}
+}
+
+// CheckPassword ...
+func (u *User) CheckPassword(phone, password string) (*UserRequest, error) {
+	data := make(url.Values)
+	data["phone"] = []string{phone}
+	data["password"] = []string{password}
+
+	url := u.uri + "/v1/ps"
+	out := &UserRequest{}
+	err := api.PostForm(url, data, out)
+	if err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
