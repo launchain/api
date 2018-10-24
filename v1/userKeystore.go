5b0b9370b770df70b6cf58c775e8cb79aad50529
周帅 <571840649@qq.com>
2018-10-24 17:04:55 +0800

    userKeystore api

---
+++ v1/userKeystore.go
@@ -0,0 +1,38 @@
+package v1
+
+import (
+	"fmt"
+	"github.com/launchain/api"
+	"net/url"
+)
+
+// UserKeystore ...
+type UserKeystore struct {
+	uri string
+}
+
+// UserKeyStoreCreateRequest ...
+type UserKeyStoreCreateRequest struct {
+	UserID   string
+	Address  string
+	KeyStore string
+	FileName string
+}
+
+// NewUserKeyStore ...
+func NewUserKeyStorer(c *api.Config) *UserKeystore {
+	c.Check()
+	uri := c.URI()
+	return &UserKeystore{uri: uri}
+}
+
+// UserKeyStoreCreate ...
+func (u *UserKeystore) UserKeyStoreCreate(uk *UserKeyStoreCreateRequest) (map[string]interface{}, error) {
+	out := make(map[string]interface{})
+	data := make(url.Values)
+	data.Add("address", uk.Address)
+	data.Add("keystore", uk.KeyStore)
+	data.Add("filename", uk.FileName)
+	url := fmt.Sprintf("%s/v1/user/%s/keystore", u.uri, uk.UserID)
+	return out, api.PostForm(url, data, &out)
+}
