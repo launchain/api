31cf92a91d79f9f8eb86636aac46ad421194d39a
周帅 <571840649@qq.com>
2018-11-07 19:28:11 +0800

    update uri

--- v1/certification.go
+++ v1/certification.go
@@ -19,7 +19,7 @@ func NewCertification(c *api.Config) *Certification {
 }
 
 // CreateChain ...
-func (c *CertificationForChain) CreateCertification(userId string) (map[string]interface{}, error) {
+func (c *Certification) CreateCertification(userId string) (map[string]interface{}, error) {
 	data := make(url.Values)
 	Url := fmt.Sprintf("%s/v1/identity/certification/%s", c.uri, userId)
 	out := make(map[string]interface{})
