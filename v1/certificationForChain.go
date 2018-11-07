108e8981f0b6937a6337e0a5bd0cf4bc8c7dd4f2
周帅 <571840649@qq.com>
2018-11-07 15:35:25 +0800

    add certification api

---
+++ v1/certificationForChain.go
@@ -0,0 +1,30 @@
+package v1
+
+import (
+	"fmt"
+	"github.com/launchain/api"
+	"net/url"
+)
+
+// Certification ...
+type Certification struct {
+	uri string
+}
+
+// CertificationRequest ..
+type CertificationRequest struct {
+	Name    string
+	Vin     string
+	Address string
+}
+
+// CreateChain ...
+func (c *Certification) CreateChain(req *CertificationRequest) (map[string]interface{}, error) {
+	data := make(url.Values)
+	data.Add("name", req.Name)
+	data.Add("address", req.Address)
+	data.Add("vin", req.Vin)
+	Url := fmt.Sprintf("%s%s", c.uri, "/v1/car/certification")
+	out := make(map[string]interface{})
+	return out, api.PostForm(Url, data, &out)
+}
