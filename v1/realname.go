e846abc6c5f3d568af33fb8c622b831dbbdf5af1
周帅 <571840649@qq.com>
2018-12-10 16:26:45 +0800

    update word error spell

--- v1/realname.go
+++ v1/realname.go
@@ -6,32 +6,32 @@ import (
 	"net/url"
 )
 
-// RealNamAuth ...
-type RealNamAuth struct {
+// RealNameAuth ...
+type RealNameAuth struct {
 	uri string
 }
 
-// RealNamAuthResponse ...
-type RealNamAuthResponse struct {
+// RealNameAuthResponse ...
+type RealNameAuthResponse struct {
 	Name   string `json:"name"`
 	IDCard string `json:"idcard"`
 	Status int    `json:"status"` //校验状态 10=一致 20=不一致
 }
 
-// NewRealNamAuth ...
-func NewRealNamAuth(c *api.Config) *RealNamAuth {
+// NewRealNameAuth ...
+func NewRealNameAuth(c *api.Config) *RealNameAuth {
 	c.Check()
 	uri := c.URI()
-	return &RealNamAuth{uri: uri}
+	return &RealNameAuth{uri: uri}
 }
 
-// RealNameAuth ...
-func (r *RealNamAuth) CheckRealNameAuth(name, idcard string) (*RealNamAuthResponse, error) {
+// CheckRealNameAuth ...
+func (r *RealNameAuth) CheckRealNameAuth(name, idcard string) (*RealNameAuthResponse, error) {
 	data := make(url.Values)
 	data.Add("name", name)
 	data.Add("idcard", idcard)
 	Url := fmt.Sprintf("%s%s", r.uri, "/v1/car/certification")
-	out := RealNamAuthResponse{}
+	out := RealNameAuthResponse{}
 	err := api.PostForm(Url, data, &out)
 	if err != nil {
 		return &out, err
