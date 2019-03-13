6d1a83654b6198ba9389eeb2baace32391609c3c
xiaohei5194 <xiaohei5194@163.com>
2019-03-13 16:54:29 +0800

    add get token flow

--- v1/tokenFlow.go
+++ v1/tokenFlow.go
@@ -1,8 +1,10 @@
 package v1
 
 import (
+	"fmt"
 	"github.com/launchain/api"
 	"net/url"
+	"time"
 )
 
 // TokenFlow ...
@@ -20,6 +22,37 @@ type CreateTokenFlowRequest struct {
 	Rule    string `json:"rule"`
 	Device  string `json:"device"`
 	Remark  string `json:"remark"`
+}
+
+//GetTokenFlowReq ...
+type GetTokenFlowReq struct {
+	TokenID string
+	Address string
+	Page    int
+	Limit   int
+}
+
+type TokenFlowSchema struct {
+	TokenID   string    `json:"tokenid"`
+	From      string    `json:"from"`
+	To        string    `json:"to"`
+	Value     string    `json:"value"`
+	TxnHash   string    `json:"txnhash"`
+	Rule      string    `json:"rule"`
+	Device    string    `json:"device"`
+	Remark    string    `json:"remark"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type TokenFlowsRes struct {
+	Count int `json:"count"`
+	Txn   []TokenFlowSchema
+}
+
+type GetTokenFlowRes struct {
+	Code    string        `json:"code"`
+	Message string        `json:"message"`
+	Data    TokenFlowsRes `json:"data"`
 }
 
 // NewTokenFlow ...
@@ -43,4 +76,16 @@ func (t *TokenFlow) CreteTokenFlow(req *CreateTokenFlowRequest) (map[string]inte
 	data.Add("remark", req.Remark)
 	out := make(map[string]interface{})
 	return out, api.PostForm(uri, data, &out)
+}
+
+//GetTokenFlow ...
+func (t *TokenFlow) GetTokenFlow(req *GetTokenFlowReq) (GetTokenFlowRes, error) {
+	out := GetTokenFlowRes{}
+	url := fmt.Sprintf("%s/v1/tokenflow/token/%s/address/%s?page=%d&limit=%d",
+		t.uri, req.TokenID, req.Address, req.Page, req.Limit)
+	err := api.Get(url, &out)
+	if err != nil {
+		return GetTokenFlowRes{}, err
+	}
+	return out, nil
 }
